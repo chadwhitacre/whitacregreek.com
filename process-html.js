c26d86a8189a221cee5259163f4839dbe38d4d19
Chad Whitacre <chad@zetaweb.com>
2023-10-26 00:23:47 -0400

    Remove scripts

--- process-html.js
+++ process-html.js
@@ -4,12 +4,30 @@ const path = require( 'path' );
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
+function modifyOne(dom) {
+  const document = dom.window.document;
+
+  function remove(el) {
+    el.parentNode.removeChild(el);
+  }
+
+  // Remove all scripts
+  var script;
+  while(1) {
+    script = document.querySelector("script");
+    if (script === null) break;
+    remove(script)
+  };
+}
+
 async function handleFile(filepath) {
+  console.log(filepath);
   if (!filepath.endsWith('.html')) {
     return;
   }
   var html;
   const dom = await JSDOM.fromFile(filepath).then(dom => {
+    modifyOne(dom);
     html = dom.serialize();
   });
   const www = filepath.replace('raw/', 'www/');
@@ -22,13 +40,15 @@ async function handleDir(dirpath) {
     const next = path.join(dirpath, name);
     const stat = await fs.promises.stat(next);
     if(stat.isFile()) {
-      handleFile(next);
+      return await handleFile(next);
     } else if(stat.isDirectory()) {
-      handleDir(next);
+      await handleDir(next);
+    } else {
+      throw aFit;
     }
   }
 }
 
 (async ()=>{
-  handleDir('raw')
+  await handleDir('raw')
 })();
